c90d988f980d1223ef45ce6ff3e7ef45d24369e8
Teddy <teddy.gauthier@outlook.com>
2022-07-20 22:12:14 +0200

    fix : page

---
+++ front-page.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+    <main id="site-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-3">
+test
+                </div>
+                <div class="col-9">
+test
+                </div>
+            </div>
+        </div>
+    </main>
+
+<?php get_footer(); ?>
---
+++ home.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+    <main id="site-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 offset-3">
+test 2
+                </div>
+            </div>
+        </div>
+    </main>
+
+<?php get_footer(); ?>
