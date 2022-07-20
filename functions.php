4b221eb3be6527548862aeb914eb182e779a299c
S.7 <ebarbosa4@myges.fr>
2022-07-20 21:48:54 +0200

    Made visual changes to searchBar

--- functions.php
+++ functions.php
@@ -76,6 +76,17 @@ function ajax_load_posts(){
     wp_die();
 }
 
+function html5_search_form( $form ) {
+    $form = '<section class="search"><form role="search" method="get" id="search-form" action="' . home_url( '/' ) . '" >
+    <label class="screen-reader-text" for="s">' . __('',  'domain') . '</label>
+     <input type="search" value="' . get_search_query() . '" name="s" id="searchinput" placeholder="Type to search" />
+     <input type="submit" id="searchsubmit" value="'. esc_attr__('Go', 'domain') .'" />
+     </form></section>';
+    return $form;
+}
+
+add_filter( 'get_search_form', 'html5_search_form' );
+
     
 
 
